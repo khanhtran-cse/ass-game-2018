c7cdd4f0222b2c463e06a83281910fae57664af2
Unknown <boss1905@outlook.com>
2018-09-13 11:04:39 +0700

    [UPDATE] menu

--- menu.py
+++ menu.py
@@ -8,29 +8,40 @@ class Menu:
 	MENUIMAGE = pygame.image.load('./images/menu.jpg')
 	MENUIMAGE = pygame.transform.scale(MENUIMAGE, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
-	def __init__(self, gameDisplay, x, y):
+	tron_yel = (255, 202, 0) 
+	tron_regular = (255,   0,   0)
+	tron_light = (255, 218,  10)
+
+	def __init__(self, gameDisplay, x1, y1, x2, y2):
 		self.gameDisplay = gameDisplay
-		self.x = x
-		self.y = y
-		self.textPlay = 'Play'
-		self.textExit = 'Exit'
-		self.backgroundPlay = (x, x + 100, y , y + 45)
-		self.backgroundExit = (x, x + 100, y + 50, y + 100)
+		self.x1 = x1
+		self.y1 = y1
+		self.x2 = x2
+		self.y2 = y2
+		self.play = ('Play', x1, y1, 50, 100, self.tron_yel )
+		self.exit = ('Exit', x2, y2 , 50, 100, self.tron_yel )
 		self.font = pygame.font.SysFont('arial', 50)
 
-	def draw(self):
 
-		textPlay = self.font.render(self.textPlay, True, Menu.TEXTCOLOR)
-		textExit = self.font.render(self.textExit, True, Menu.TEXTCOLOR)
-		self.gameDisplay.blit(textPlay, (self.x + 15, self.y + 5 ))
-		self.gameDisplay.blit(textExit, (self.x + 15, self.y + 50 ))
+	def make_button(self, text, xpo, ypo, height, width, colour):
+	    pygame.draw.rect(self.gameDisplay, self.tron_regular,
+	                     (xpo - 10, ypo - 10, width, height), 3)
+	    pygame.draw.rect(self.gameDisplay, self.tron_light,
+	                     (xpo - 9, ypo - 9, width - 1, height - 1), 1)
+	    pygame.draw.rect(self.gameDisplay, self.tron_regular,
+	                     (xpo - 8, ypo - 8, width - 2, height - 2), 1)
+	    font = pygame.font.Font(None, 42)
+	    label = font.render(str(text), 1, (colour))
+	    self.gameDisplay.blit(label, (xpo, ypo))
+
+	def draw(self):
+		self.make_button(*self.play)
+		self.make_button(*self.exit)
 
 	def checkPos(self, x, y):
-		print(self.backgroundPlay);
-		print(self.backgroundExit)
-		if self.backgroundPlay[0] < x < self.backgroundPlay[1] and self.backgroundPlay[2]< y <self.backgroundPlay[3]:
+		if self.play[1] < x < self.play[1]+ self.play[4] and self.play[2]< y <self.play[2] + self.play[3]:
 			return 'play'
-		elif(self.backgroundExit[0] < x < self.backgroundExit[1] and self.backgroundExit[2]< y < self.backgroundExit[3]):
+		elif(self.exit[1] < x < self.exit[1] + self.exit[4] and self.exit[2]< y < self.exit[2] + self.exit[3]):
 			return 'exit'
 
 def startMenu():
@@ -40,7 +51,7 @@ def startMenu():
 	# Set the size for the window
 	gameDisplay = pygame.display.set_mode(WINDOW_SIZE)
 
-	menu = Menu(gameDisplay, 450, 300)
+	menu = Menu(gameDisplay, 460, 350, 460, 410)
 	pygame.display.set_caption('Menu')
 	menu.draw()
 
@@ -56,7 +67,6 @@ def startMenu():
 					startTank(gameDisplay)
 					pass
 
-				# print(res, posX, posY, exit)
 		
 		gameDisplay.blit(Menu.MENUIMAGE, (0,0))
 		menu.draw()
